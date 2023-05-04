0e22e5232cb1d653b6079330f307ffa08a881a0b
HaekyungSeong <haekyungs95@gmail.com>
2023-05-04 21:57:55 +0900

    set 말고 helper function 으로 type 제어

--- utility-module/class.js
+++ utility-module/class.js
@@ -4,34 +4,56 @@ class Calculator {
     this.y = b;
   }
 
-  set x(value) {
-    if (!Number.isInteger(value)) {
-      return console.log('x가 정수가 아닙니다. 정수를 입력해주세요')
-    }
-    this._x = value
-  }
+  // set x(value) {
+  //   if (!Number.isInteger(value)) {
+  //     return console.log('x가 정수가 아닙니다. 정수를 입력해주세요')
+  //   }
+  //   this._x = value
+  // }
 
-  set y(value) {
-    if (!Number.isInteger(value)) {
-      return console.log('y가 정수가 아닙니다. 정수를 입력해주세요')
+  // set y(value) {
+  //   if (!Number.isInteger(value)) {
+  //     return console.log('y가 정수가 아닙니다. 정수를 입력해주세요')
+  //   }
+  //   this._y = value
+  // }
+
+  isInteger(){
+    if(Object.values(this).every((value)=>{
+      return Number.isInteger(value)
+    })){
+      this._x=this.x;
+      this._y=this.y;
+
+      return true
+    }else{
+      console.log('매개변수는 정수만 가능합니다. 정수를 입력하세요')
+      return false
     }
-    this._y = value
   }
 
   add() {
-    return this._x + this._y
+    if(this.isInteger()){
+      return this._x + this._y
+    }
   }
 
   minus() {
-    return this._x - this._y
+    if(this.isInteger()){
+      return this._x - this._y
+    }
   }
 
   divide() {
+    if(this.isInteger()){
     return this._x / this._y
+    }
   }
 
   multiply() {
+    if(this.isInteger()){
     return this._x * this._y
+    }
   }
 
   get allCalaulator() {
